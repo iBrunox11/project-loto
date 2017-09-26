ace9a4b6c4c3b262dd6ff3ee6d19395b3904abff
Bruno <bruninhopcs@hotmail.com>
2017-09-26 14:14:45 -0300

    continue with this at home

--- layouts/index.php
+++ layouts/index.php
@@ -3,3 +3,16 @@
 //pagina main*
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto Gerador de Números</title>
+</head>
+<body>
+<form action="formulario.php" action="POST">
+	<input type="" name="">
+	<input type="submit" name="enviar" alt="Enviar">
+</form>
+</form>
+</body>
+</html>
