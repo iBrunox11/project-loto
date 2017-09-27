1deee866ac6cc6f80437d841e43fd352f6b4e2a8
Bruno Dias <bruninhopcs@hotmail.com>
2017-09-27 00:31:30 -0300

    loading page with some errors to fix

--- layouts/index.php
+++ layouts/index.php
@@ -7,12 +7,76 @@
 <html>
 <head>
 	<title>Projeto Gerador de Números</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous"> 
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<script>
+		
+		window.onload = function() {
+     var segundos = 4;
+     setTimeout(function () {
+         var hidding = document.getElementById("hidding");
+         var show = document.getElementById("hid");
+         show.style.display.inline;
+         hidding.className = ""; //Remove a classe hide
+     }, segundos * 1000);
+};
+
+	</script>
+	<style>
+		
+		.loader {
+    border: 16px solid #f3f3f3; /* Light grey */
+    border-top: 16px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+}
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+.container {
+	padding-top: 10%;
+	text-align: center;
+
+}
+.container .loader{
+	margin: 0 auto;
+
+}
+
+	</style>
+
+		
+
 </head>
 <body>
-<form action="formulario.php" action="POST">
-	<input type="" name="">
-	<input type="submit" name="enviar" alt="Enviar">
+<div class="container">
+<h1>Projeto integrado</h1>
+<div class="loader" id="hidding"></div>
+</div>
+
+<div id="hid" class="container" style="display: none;">
+	<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      Check me out
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-</form>
+</div>
+
 </body>
 </html>
