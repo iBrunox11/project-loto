53a15f020ff22eacd59ea5b48f2eafa52ea4f95a
Bruno Dias <bruninhopcs@hotmail.com>
2017-09-29 00:24:36 -0300

    new features and layouts

--- layouts/index.php
+++ layouts/index.php
@@ -1,7 +1,4 @@
 <?php 
-
-//pagina main*
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,17 +13,20 @@
      setTimeout(function () {
          var hidding = document.getElementById("hidding");
          var show = document.getElementById("hid");
-         show.style.display.inline;
-         hidding.className = ""; //Remove a classe hide
+         show.style = "display:inline"; //exibe o formulário
+         hidding.className = ""; //Remove o load
      }, segundos * 1000);
 };
+  function entrar(){
+};
 
+ 
 	</script>
 	<style>
 		
 		.loader {
-    border: 16px solid #f3f3f3; /* Light grey */
-    border-top: 16px solid #3498db; /* Blue */
+    border: 16px solid #f3f3f3; /* Cor da borda */
+    border-top: 16px solid #3498db; /* Cor do load */
     border-radius: 50%;
     width: 120px;
     height: 120px;
@@ -46,6 +46,11 @@
 
 }
 
+
+ #hid {
+  display: none;
+}
+
 	</style>
 
 		
@@ -57,24 +62,24 @@
 <div class="loader" id="hidding"></div>
 </div>
 
-<div id="hid" class="container" style="display: none;">
-	<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+<div id="hid" class="container">
+	<form method="POST" >
+  <div  class="form-group">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Email">
+    <!-- <small id="emailHelp" class="form-text text-muted">Entre com seus dados de acesso</small> -->
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="exampleInputPassword1">Senha</label>
+    <input type="password" class="form-control" name="senha" id="exampleInputPassword1" placeholder="Senha">
   </div>
   <div class="form-check">
     <label class="form-check-label">
-      <input type="checkbox" class="form-check-input">
-      Check me out
+      <input type="checkbox"  class="form-check-input">
+      Não sou um robô
     </label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" id="btnEntrar" onclick="entrar()" class="btn btn-primary">Entrar</button>
 </form>
 </div>
 
